fd1c93c5e2a228a1deb26b2c8bfb5c60083de071
Tamagna Davide <supernikx@gmail.com>
2018-10-16 11:02:12 +0200

    Aggiunto background manager per gestire scorrimento background

---
+++ Assets/Script/BackGround/BackgroundController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+
+    float screenWidth;
+    Bounds bound;
+    BackgroundManager bgManager;
+
+    private bool CheckScreenPosition()
+    {
+        if (bound.extents.x + transform.position.x < -screenWidth)
+            return true;
+        return false;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        bgManager = FindObjectOfType<BackgroundManager>();
+        screenWidth = (Camera.main.orthographicSize * Camera.main.aspect);
+        bound = GetComponent<SpriteRenderer>().bounds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CheckScreenPosition())
+            bgManager.RespawnBG(this);
+    }
+}
---
+++ Assets/Script/BackGround/BackgroundManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour {
+    public float backgoundDislpace = 40.90001f;
+    public float backgroundspeed=0.1f;
+    public BackgroundController bg1;
+    public BackgroundController bg2;
+    BackgroundController currentBG;
+
+	// Use this for initialization
+	void Start () {
+        currentBG = bg1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(-backgroundspeed, 0, 0);
+	}
+
+    public void RespawnBG(BackgroundController bg)
+    {
+        if (bg != currentBG)
+            return;
+        BackgroundController other = (bg == bg1) ? bg2 : bg1;
+        bg.transform.position = new Vector3(other.transform.position.x + backgoundDislpace, other.transform.position.y, other.transform.position.z);
+        currentBG = other;
+    }
+}
