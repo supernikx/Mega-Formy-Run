3194ff36c2233a5084d3bf10970685c3f7d6d5db
Tamagna Davide <supernikx@gmail.com>
2018-10-22 11:22:25 +0200

    Aggiunta logica audio e scarpa bianca con animazioni

---
+++ Assets/Script/Manager/SoundManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+    [SerializeField]
+    AudioSource Source;
+    [SerializeField]
+    AudioClip StartClip;
+    [SerializeField]
+    AudioClip LoopClip;
+
+    void Start () {
+        StartCoroutine(StartAudio());
+	}
+
+    IEnumerator StartAudio()
+    {
+        Source.clip = StartClip;
+        Source.Play();
+        yield return new WaitForSeconds(StartClip.length);
+        Source.clip = LoopClip;
+        Source.Play();
+        Source.loop = true;
+    }
+
+}
